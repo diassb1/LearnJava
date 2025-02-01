0e08aff2489942a64869aa329e422c234ab37142
Dias Sadykov <diassb@mail.ru>
2025-02-01 19:10:35 +0500

    lesson 25, homework, new branch

--- src/Lesson25/Student.java
+++ src/Lesson25/Student.java
@@ -3,22 +3,48 @@ package Lesson25;
 public class Student {
     private StringBuilder name;
     private int course;
-    private double grade;
+    private int grade;
 
     public StringBuilder getName(){
+        StringBuilder sb = new StringBuilder(this.name);
         //new line for git 2
-        return name;
+        return sb;
     }
 
     public int getCourse() {
         return course;
     }
 
-    public double getGrade() {
-        return getGrade();
+    public int getGrade() {
+        return grade;
     }
 
 
+    public void setName(StringBuilder name) {
+        if (name.length() > 3 ) {
+            this.name = name;
+        } else this.name = new StringBuilder("no name");
+    }
+
+    public void setCourse (int course) {
+        if (course >= 1 && course <= 4) {
+            this.course = course;
+        }
+    }
+
+    public void setGrade (int grade) {
+        if (grade >=1 && grade <=10) {
+            this.grade = grade;
+        }
+    }
+
+    public void showInfo() {
+        System.out.printf("""
+                Имя студента: %s
+                Курс: %d
+                Оценка: %d
+                """, getName(), getCourse(), getGrade());
+    }
 
 }
 
